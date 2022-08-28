f5082add7ad1f0d126d2832f934f8a948e55c991
MobinaAzadi <mobina.azadi75@gmail.com>
2022-08-28 09:24:52 +0430

    git commet -m "added AMRAP"

--- CrossFitClock_V10/main.c
+++ CrossFitClock_V10/main.c
@@ -183,6 +183,8 @@ main()
 		
 		}
 
+		
+
 		else if(work_Mode == MENU )
 		{
 			time_UnNormal = 0;
@@ -1454,7 +1456,7 @@ main()
 		else if(work_Mode == AMRAP) /// as many roand aspossible
 		{
 			time_UnNormal = 0;
-			
+
 
 			segments_value_present[0] = '0';
 			segments_value_present[1] = '0';
